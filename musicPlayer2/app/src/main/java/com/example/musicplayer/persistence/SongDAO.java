478b2ad6cc1c152aff7990fce0f0fe19593cc10f
Katelyn Suhr <katy@suhr.com>
2020-03-02 01:30:35 -0800

    Created Room Persistence Data Access Objects  (DAO)

--- musicPlayer2/app/src/main/java/com/example/musicplayer/persistence/SongDAO.java
+++ musicPlayer2/app/src/main/java/com/example/musicplayer/persistence/SongDAO.java
@@ -1,4 +1,30 @@
 package com.example.musicplayer.persistence;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.musicplayer.models.Song;
+
+import java.util.List;
+
+@Dao
 public interface SongDAO {
+    @Insert
+    long[] insertSongs(Song... songs); // return long array that has all rows, if insert fails returns -1
+
+    @Query("SELECT * FROM songs")
+    LiveData<List<Song>> getSongs(); //livedata is data observation class
+
+//    @Query("SELECT * FROM songs WHERE id= :id")
+//    List<Song> getSongWithCustomQuery(String id);
+
+    @Delete
+    int delete(Song... songs);
+
+    @Update
+    int update(Song... songs); // returns integer that is hows many rows were affeected
 }
